d063009aa7acfa2dc22f05ff6c2d21e5b66c800e
computerTop <xerucus@yandex.ru>
2022-05-25 19:28:45 +0300

    Первый коммит src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavPan from "./Components/NavPan/NavPan";
+import Tecnologes from "./Components/Tecnologes/Tecnologes";
+
+function App() {
+  return (
+    <div className="App">
+      <NavPan />
+      <Tecnologes />
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/Components/NavPan/NavPan.js
@@ -0,0 +1,15 @@
+
+
+
+function NavPan(){
+  return(
+  <div className="nav">
+    <a href="">Главная страница </a>
+    <a href=""> О нас </a>
+    <a href="">Контакты </a>
+    <a href=""> Магазин</a>
+  </div>
+  );
+}
+
+export default NavPan;
