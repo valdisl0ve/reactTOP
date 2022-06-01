1f1eb1ec1548506a439d7d65ff2e298036d29498
valdisl0ve <75386722+valdisl0ve@users.noreply.github.com>
2022-06-01 19:11:25 +0300

    +

--- src/Components/Nav_box/Nav_box.js
+++ src/Components/Nav_box/Nav_box.js
@@ -7,7 +7,7 @@ import {NavLink, Router} from "react-router-dom";
 function NavBox(){
   return(
     <div className="Nav_box">
-        <NavLink to="/test">123</NavLink>
+        <NavLink to="/test">123</NavLink>;
     </div>
 
   );
